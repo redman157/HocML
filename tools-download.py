30c6c3519d2915c325377e4ce2178a84f4267091
redman157 <32203726+redman157@users.noreply.github.com>
2018-10-26 11:51:03 +0700

    Update tools-download.py

--- tools-download.py
+++ tools-download.py
@@ -1,10 +1,14 @@
+from __future__ import division, print_function
 import pandas as pd
-def download(filename):
-  url = ''
-  path = url.download()
-  
-def type(path):
-  dict = {}
-  file = pd.read_csv(path).splitlines()
-  file.pop(0)
+import matplotlib.pyplot as plt
+import requests
+import os 
+import csv
+path = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
+filename = (path.split('/')[-1])
+dir = os.path.join(os.path.abspath(os.path.dirname(filename)))
+r = requests.get(path,allow_redirects=True, stream = True,verify = False)
+if r.status_code == 200:    
+  with open(dir + '/' + filename ,'wb') as f:
+    f.write(r.content)
     
