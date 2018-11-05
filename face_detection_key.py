619f892762b962db2097b043f74f3a136feb31ea
redman157 <32203726+redman157@users.noreply.github.com>
2018-11-05 13:22:37 +0700

    Update face_detection_key.py

--- face_detection_key.py
+++ face_detection_key.py
@@ -1,11 +1,13 @@
 from tensorflow import keras
-from keras.layers import Dense
+from keras import Sequential
+from keras.layers import Dense, Flatten, Activation
 import tensorflow as tf
-import csv
 import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
 from sklearn.model_selection import train_test_split, cross_validate
+import pickle
+import os
 %matplotlib inline
 IMAGE_WIDTH = 96
 IMAGE_HEIGHT = 96
@@ -14,14 +16,25 @@ records = reader.Image
 inputs = reader.iloc[:,:-2]
 
 records = records.values
-print(records.shape)
 inputs = inputs.values
-images = None
-for i in range(records.shape[0]):
+def load_dataset():
+  images = None
+  for i in range(0,records.shape[0]):
     if images is None:
-        images = np.array(records[i].split(' '), dtype=np.int32)
+      images = np.array(records[i].split(' '), dtype=np.int32)
     else:
-        images = np.concatenate((images, np.array(records[i].split(' '), dtype=np.int32)), axis=0)
-else:
-    print(images.reshape(96,96,1))
-    print(inputs.shape)
+      images = np.concatenate((images, np.array(records[i].split(' '), dtype=np.int32)), axis=0)
+  else:
+    return images
+def load_data():
+  if os.path.exists("train") is False:
+    data = load_dataset()
+    with open("train","wb") as f:
+      pickle.dump(load_dataset(),f)
+    f.close()
+  else:
+    with open("train","rb") as f:
+      data = pickle.load(f)
+    f.close()
+  return data
+
